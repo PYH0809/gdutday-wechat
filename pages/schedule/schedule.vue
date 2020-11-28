727bfac624b4ae390080e85aad01aea2702a7878
Cerbur <1051140642@qq.com>
2020-11-29 00:33:15 +0800

    修复老用户绑定提示

--- pages/schedule/schedule.vue
+++ pages/schedule/schedule.vue
@@ -64,6 +64,7 @@ export default {
 	onLoad(query) {
 		uni.showShareMenu();
 		this.update();
+        this.checkOldUser();
 		if (query.params !== undefined) {
 			this.showModal({
 				type: 'bottomModal',
@@ -159,7 +160,26 @@ export default {
 				}
 			});
 			// #endif
-		}
+            
+		},
+        checkOldUser() {
+            var version = uni.getStorageSync('version');
+            var id = this.$education.ID;
+            if (id == "" && version != "1.0.0") {
+                let that = this;
+                uni.showModal({
+                	title: '提示',
+                	content: '检查到您是老用户,由于特殊原因gdutday转移使用教务系统登录,劳烦绑定教务系统登录,以保证正常使用',
+                	confirmColor: this.$commonFun.hexify(this.$colorList.theme),
+                	confirmText: '前往',
+                    success (res) {
+                        if(res.confirm) {
+                            that.$Router.push({ name: 'login' });
+                        }
+                    }
+                });
+            }
+        }
 	}
 };
 </script>
